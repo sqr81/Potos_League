0439e1307536c200768ba627ade43509a99f4461
sqr81 <sqr81@free.fr>
2019-12-04 16:53:20 +0100

    nouveau Json debut scss

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,13 +1,9 @@
 <?php
-
-
 namespace App\Controller;
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
 class IndexController extends AbstractController
 {
     /**
@@ -19,7 +15,6 @@ class IndexController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
         return $this->render('index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/main", name="main")
      */
     public function index()
     {
